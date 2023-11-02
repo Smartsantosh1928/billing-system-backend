cddef49417bc855e72c949e788411b5c08faefd4
smartsantosh1928 <smartsantosh1928@gmail.com>
2023-11-02 21:15:37 +0530

    back

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -13,23 +13,16 @@ router.post('/register',(req,res) => {
         if(user) return res.json({ success: false, msg: "User already exists!" });
         else{
             const role='cashier',isActive=false,otp=generateOTP();
-            const accessToken = jwt.sign({name,email,role,isActive},process.env.ACCESS_TOKEN_SECRET,
-                {
-                expiresIn: '1h', 
-                issuer: 'http://localhost:3000', 
-                }
-            );
-            const refreshToken = jwt.sign({name,email,role,isActive},process.env.REFRESH_TOKEN_SECRET);
             const user = { 
                 name,email,password: hashedPassword,
                 isActive: false,role: 'cashier',
-                refreshToken,createdAt: new Date(),
+                refreshToken:"",createdAt: new Date(),
                 updatedAt: new Date(),otp
             };
             User.create(user).then(user => {
                 sendMail(user.email,'OTP for Cashier Registration',`Your OTP is ${otp}`)
                 .then(() => {
-                    res.json({ success: true, msg: "User Registered Successfully!",accessToken,refreshToken });
+                    res.json({ success: true, msg: "User Registered Successfully!"});
                 })
                 .catch(err => {
                     res.json({ success: false, msg: err.message });
@@ -41,14 +34,22 @@ router.post('/register',(req,res) => {
 
 router.post('/verifyOTP',(req,res) => {
     const { otp, email } = req.body;
-    // const { email } = req.user;
     User.findOne({ email }).then(user => {
         if(!user) return res.json({ success: false, msg: "User not found!" });
         else{
             if(user.otp == otp){
                 user.isActive = true;
+                const { name,email,role,isActive } = user;
+                const accessToken = jwt.sign({name,email,role,isActive},process.env.ACCESS_TOKEN_SECRET,
+                    {
+                    expiresIn: '1h', 
+                    issuer: 'http://localhost:3000', 
+                    }
+                );
+                const refreshToken = jwt.sign({name,email,role,isActive},process.env.REFRESH_TOKEN_SECRET);
+                user.refreshToken = refreshToken;
                 user.save().then(() => {
-                    res.json({ success: true, msg: "OTP Verified Successfully!" });
+                    res.json({ success: true, msg: "OTP Verified Successfully!" ,accessToken,refreshToken});
                 })
             }
             else return res.json({ success: false, msg: "Invalid OTP!" });
@@ -76,7 +77,14 @@ router.post('/login',(req,res) => {
     })
 })
 
-router.post('/getRefreshToken',(req,res) => {
+router.post('/verifyUser',verifyToken,(req,res) => {
+    const user = req.user;
+    if(!user) return res.json({ success: false, msg: "User not found!" });
+    else if(!user.isActive) return res.json({ success: false, msg: "User not verified!" });
+    else return res.json({ success: true, msg: "User Verified Successfully!",role: user.role});
+})
+
+router.post('/getAccessToken',(req,res) => {
     const { refreshToken } = req.body;
     if(refreshToken == null) return res.sendStatus(401);
     User.findOne({ refreshToken }).then(user => {
@@ -84,7 +92,7 @@ router.post('/getRefreshToken',(req,res) => {
         else{
             const { name,email,role,isActive } = user;
             const accessToken = jwt.sign({ name,email,role,isActive }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '3600s' });
-            res.json({ accessToken });
+            res.json({ success: true, accessToken });
         }
     })
 })
