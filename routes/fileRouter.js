961b293c8dcbc44f0d9bfe8573973a16a7c57ac6
smartsantosh1928 <smartsantosh1928@gmail.com>
2023-11-04 12:36:51 +0530

    files

--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 const fs = require('fs');
-
+ 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, 'uploads/'); 
