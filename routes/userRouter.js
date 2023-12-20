c976215abca1e46c24794298a9b0f84d9042e8c6
test <test@UGSERVER.com>
2023-12-20 11:14:38 +0530

    modifyuserrecord2

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require('../models/userModel');
 
-router.post('/allusers',(req,res)=>{
+router.get('/allusers',(req,res)=>{
     User.find().then(users=>{
         if(users==null)
             return res.json({status:false,msg:"No users in the database"})
